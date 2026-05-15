2f8cc8441fa45ee4e19152bc0b37f74feff2d3be
sewid <sewidat@gmailc.om>
2026-05-15 22:21:02 +0300

    Implement twoSum method in P1TwoSum class and uncomment test examples

--- src/main/java/org/example/arrays/P1TwoSum.java
+++ src/main/java/org/example/arrays/P1TwoSum.java
@@ -54,15 +54,23 @@ public class P1TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
         // TODO: implement
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i <nums.length ; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(Math.abs(target-nums[i]),i);
+
+        }
         return new int[]{};
     }
 
     public static void main(String[] args) {
         P1TwoSum sol = new P1TwoSum();
         // Example 1: expected [0, 1]
-        // System.out.println(Arrays.toString(sol.twoSum(new int[]{2,7,11,15}, 9)));
+         System.out.println(Arrays.toString(sol.twoSum(new int[]{2,7,11,15}, 9)));
         // Example 2: expected [1, 2]
-        // System.out.println(Arrays.toString(sol.twoSum(new int[]{3,2,4}, 6)));
+         System.out.println(Arrays.toString(sol.twoSum(new int[]{3,2,4}, 6)));
         System.out.println("P1TwoSum — uncomment examples above to test.");
     }
 }
